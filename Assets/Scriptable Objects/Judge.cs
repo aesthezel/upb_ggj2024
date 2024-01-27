c837b82a2cb9c5cb5b8b4c723a53d448a40d1c65
Lester-Cano <Nestor.cano@timba.co>
2024-01-26 23:04:07 -0500

    Cinemachine and judge

--- Assets/Scriptable Objects/Judge.cs
+++ Assets/Scriptable Objects/Judge.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Judge : MonoBehaviour
+public class Judge : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    public string name
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
