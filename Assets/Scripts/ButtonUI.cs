fe4aa508b70809870ca1c6ea487bc9f600c2eef7
2CB <mateojimenezfamaart@gmail.com>
2024-01-27 00:42:09 -0500

    Ui Template done

---
+++ Assets/Scripts/ButtonUI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonUI : MonoBehaviour
+{
+ //Funcion para cada boton
+
+    public void Q()
+    {
+        Debug.Log("Presionaste la Q");
+    }
+
+    public void W()
+    {
+        Debug.Log("Presionaste la W");
+    }
+
+    public void E()
+    {
+        Debug.Log("Presionaste la E");
+    }
+
+    public void R()
+    {
+        Debug.Log("Presionaste la R");
+    }
+
+    public void T()
+    {
+        Debug.Log("Presionaste la T");
+    }
+
+}
