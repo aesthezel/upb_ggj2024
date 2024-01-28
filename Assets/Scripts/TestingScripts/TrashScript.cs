8982f3f84a453e244506631d65e4273e05fd6b83
AlonsoMejiaA <almear01@gmail.com>
2024-01-28 00:06:17 -0500

    test things and audio manager

---
+++ Assets/Scripts/TestingScripts/TrashScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashScript : MonoBehaviour
+{
+    List<int> list = new List<int>();
+    private void Update()
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+
+        }
+    }
+
+}
